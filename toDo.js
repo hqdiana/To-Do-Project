d94edd39c0c2efee838a75ccb964fe62aa794590
Diana Qureshi <259169993+hqdiana@users.noreply.github.com>
2026-03-05 15:55:56 -0500

    create a function to render tasks and display them

--- toDo.js
+++ toDo.js
@@ -4,6 +4,64 @@ let storedTask = [];
 // locate html element that will contain the task list items
 const taskList = document.querySelector('.taskList')
 
+// create function to render tasks on display
+function renderTasks() {
+
+    // set default html task list element as empty
+    taskList.innerHTML = "";
+    let tasksCounter = 0;
+
+    // loop through the task list array
+    for (let i = 0; i < storedTask.length; i++) {
+
+        // for each value of the array create needed html elements
+            var div = document.createElement('div');
+            var para = document.createElement('p');
+            var paraDate = document.createElement('p');
+
+        // attach stored ID to html element
+            div.setAttribute('data-id', storedTask[i].id);
+
+        // assign classes to said html elements to modify them later in css
+            div.classList.add('taskItem');
+            para.classList.add('taskName');
+            paraDate.classList.add('taskDate')
+
+        // add text content to display to each html element 
+            para.textContent = storedTask[i].task;
+            paraDate.textContent = storedTask[i].date; 
+
+        // add childs elements to parents to display them
+            div.appendChild(para);
+            div.appendChild(paraDate);
+            taskList.appendChild(div);
+
+        // if task is active, add a done button to it, else do not add it
+            if (currentView === 'active') {
+            var button = document.createElement('button')
+            button.classList.add('doneBtn');
+            button.textContent = '✔';
+            div.appendChild(button);
+            }
+        }
+    }
+        // if the storage is empty display image based on progress
+        if (tasksCounter === 0 && currentView === 'active') {
+            // if no progress display add task img
+            if (progress === 0) {
+                var divEmpty = document.createElement('div');
+                divEmpty.classList.add('emptyMessage');
+            // if there is progress display add more task img
+                taskList.appendChild(divEmpty);
+            } else if (progress > 0) {
+                var divEmpty = document.createElement('div');
+                divEmpty.classList.add('addMore');
+                taskList.appendChild(divEmpty);
+            }
+        } 
+
+
+
 // create a function to retrieve input values + store them 
 function createTaskList() {
     // locate the add button
@@ -51,6 +109,8 @@ function createTaskList() {
             // reset input fields to empty after the button is clicked
             taskInput.value = "";
             dateInput.value = "";
+
+            renderTasks();
         }
     }) 
 
