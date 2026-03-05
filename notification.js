a6469f6b3c8e1ca97d42710ac8c69cf6749961d2
Diana Qureshi <259169993+hqdiana@users.noreply.github.com>
2026-03-05 17:26:01 -0500

    setInterval between display of task notif and page name

--- notification.js
+++ notification.js
@@ -6,6 +6,31 @@ function notification () {
         document.title = `(${taskCount}) Tasks`
     } 
     else document.title = 'Progress Tracker';
+
+    setFlashing();
+}
+
+let flashing = null;
+
+function setFlashing () {   
+    if (flashing !== null) return;
+
+        flashing = setInterval(function() {
+            let tasks = document.querySelectorAll('.taskItem');
+            let taskCount = tasks.length;
+
+            if (taskCount > 0) {
+                if (document.title === 'Progress Tracker') { 
+                    document.title = '(' + taskCount + ') Tasks'; 
+                } else { document.title = 'Progress Tracker'; }
+            }
+
+            else {
+            clearInterval(flashing);
+            flashing = null;
+            document.title = 'Progress Tracker';
+                }
+        }, 1500)
 }
 
 notification();
