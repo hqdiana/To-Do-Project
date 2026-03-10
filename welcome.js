54196b4ab726c7fd38b0da97808918b599c70599
Diana Qureshi <259169993+hqdiana@users.noreply.github.com>
2026-03-10 13:57:29 -0400

    add comments to code blocks

--- welcome.js
+++ welcome.js
@@ -14,33 +14,32 @@ const messages = [
     "what reward would I offer myself once they are achieved?"
 ];
 
-existingReward = localStorage.getItem('storedReward') 
-    if (existingReward != null){
-        window.location.href = "index.html"
-    };
-
 let index = 0;
 let storedReward = '';
 
+// if a reward input already exists, direct user to toDo.html 
+existingReward = localStorage.getItem('storedReward') 
+    if (existingReward != null){
+        window.location.href = "toDo.html"
+    };
+
+// add listener on page load to animation first welcome message
 window.addEventListener('load', () => {
     setTimeout(() => {
         text.style.opacity = "1";
     }, 5000);
 });
 
+// add listener on button to slide the welcome messages
 btn.addEventListener('click', function() {
-
     text.style.opacity = "0";
-
     setTimeout(() => {
         index++;
 
         if (index >= messages.length) {
             index = messages.length - 1;
         }
-
         text.textContent = messages[index];
-
         requestAnimationFrame(() => {
             text.style.opacity = "1";  
         });
@@ -52,11 +51,10 @@ btn.addEventListener('click', function() {
           reward.style.pointerEvents = "all"; 
         }, 1000) 
         }
-        
     }, 400);
-
 }); 
 
+// add reward input into local storage
 startButton.addEventListener('click', function() {
     let reward = storedReward = userInput.value;
     localStorage.setItem('storedReward', reward);
